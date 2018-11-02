1de588324ddeaecaf6ab6169f8b611fec8c10d53
theissp <theissp@gmail.com>
2018-11-02 14:19:28 +0100

    lidt front end på course

--- WSL/wwwroot/js/components/coursePage/course-page.js
+++ WSL/wwwroot/js/components/coursePage/course-page.js
@@ -1,8 +1,17 @@
 ﻿define(['knockout', 'broadcaster', 'dataservice'], function (ko, bc, dataservice) {
     return function (params) {
 
-        return {
+        var courses = ko.observable();
 
+        var getCourses = function () {
+            dataservice.getCourses(data => {
+                courses(data.courses);
+            });
+        }
+
+
+        return {
+            courses;
         };
 
     }
